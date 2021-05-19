a214b8f7f732784bea405e0c64b091d8d8c1e0ae
Jet <j3tmarch@gmail.com>
2021-05-19 22:15:51 +0300

    Добавлено объявление метода is_keyword_exists_in_module

--- bot_module.py
+++ bot_module.py
@@ -5,14 +5,21 @@ from abc import ABC
 """
 Базовый класс модуля для бота.
 Каждый модуль должен наследовать его.
-Это должно исключить случаи отсутствия реализации основных методов
 """
 class BotModule(ABC):
+    keywords = []
+
     def __init__(self) -> None:
         pass
 
     @abstractmethod
     def update(self):
         pass
+
+    def is_keyword_exists_in_module(self) -> bool:
+        #Перебираем все keywords. Если находим, то возвращаем true
+        #Пока ещё не придумал лаконичный способ передачи функционала vk_api в каждый модуль
+        #Поэтому это простая заглушка на данный момент
+        return True
 
 
