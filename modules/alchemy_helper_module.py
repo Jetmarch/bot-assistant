3536288e5c010c1f6bb41c16272cb49182d36789
Jet <j3tmarch@gmail.com>
2021-07-03 13:45:33 +0300

    Скелет состояний модуля AlchemyHelper

--- modules/alchemy_helper_module.py
+++ modules/alchemy_helper_module.py
@@ -6,11 +6,22 @@ class AlchemyHelperModule(BotModule):
     def __init__(self, vk, db) -> None:
         super().__init__(vk, db)
         self.keywords.extend(['алхимия', 'варки', 'алхим'])
+        self.module_name = 'AlchemyHelper'
+        self.module_states = {
+            None: self.first_stage,
+            '1':self.first_stage,
+            '2': self.second_stage
+        }
 
     def update(self):
         return super().update()
 
     def process_request(self, event):
+        module_state_for_current_user = self.db.get_user_state(self.module_name, event.user_id)
+        print(module_state_for_current_user)
+        self.module_states[module_state_for_current_user](event)
+
+        """
         user_arguments = []
         for word in event.text.split():
             if word.isdigit():
@@ -19,4 +30,13 @@ class AlchemyHelperModule(BotModule):
         count_of_ingredients = 5
         total_cost = user_arguments[0] * count_of_ingredients * catalyst_cost
 
-        self.vk.send_message(event.user_id, 'Общие затраты {0}'.format(total_cost))  
+        self.vk.send_message(event.user_id, 'Общие затраты {0}'.format(total_cost))
+        """
+
+    def first_stage(self, event):
+        self.vk.send_message(event.user_id, 'Доброй пожаловать в модуль алхимии!\n Укажите количество затраченного катализатора')
+        self.db.set_user_state(self.module_name, event.user_id, '2')
+
+    def second_stage(self, event):
+        self.vk.send_message(event.user_id, 'Заглушка для второй стадии')
+        self.db.set_user_state(self.module_name, event.user_id, '1')
