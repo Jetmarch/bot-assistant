7df1ff7e7b2857cc3d02080bfd775323d3724824
Jet <j3tmarch@gmail.com>
2021-07-03 13:00:00 +0300

    Добавлена таблица состояний модулей для каждого из пользователей. Добавлены get/set/create методы для таблицы с конфигом, так же get/set методы для таблицы состояний модулей

--- db_wrapper.py
+++ db_wrapper.py
@@ -6,7 +6,9 @@ class DB_Wrapper:
     def __init__(self) -> None:
         self.con = sqlite3.connect('bot_memory.db')
         self.cur = self.con.cursor()
+        self.create_database_for_a_bot()
         atexit.register(self.cleanup)
+
     
     def cleanup(self):
         if self.con:
@@ -38,12 +40,38 @@ class DB_Wrapper:
     def create_database_for_a_bot(self):
         try:
             self.execute_and_commit('CREATE TABLE IF NOT EXISTS CONFIG(id integer PRIMARY KEY AUTOINCREMENT, config_field text NOT NULL, config_value text)')
+            self.execute_and_commit('CREATE TABLE "ModuleStates" ("id" INTEGER NOT NULL UNIQUE,"module" TEXT NOT NULL UNIQUE,"user_id" TEXT NOT NULL,"state" TEXT NOT NULL,PRIMARY KEY("id" AUTOINCREMENT));')
         except Exception as e:
             Logger.log('ERROR', e)
     
     def get_config_value(self, config_field) -> str:
         try:
-            res = self.execute('SELECT config_value FROM CONFIG WHERE config_field = "' + str(config_field) + '"')
+            res = self.execute('SELECT config_value FROM CONFIG WHERE config_field = "{0}"'.format(config_field))
             return res.fetchone()[0]
+        except Exception as e:
+            Logger.log('ERROR', e)
+
+    def create_config_value(self, config_field, config_value):
+        try:
+            self.execute_and_commit('INSERT INTO CONFIG(config_field, config_value) VALUES ("{0}", "{1}")'.format(config_field, config_value))
+        except Exception as e:
+            Logger.log('ERROR', e)
+    
+    def set_config_value(self, config_field, config_value):
+        try:
+            self.execute_and_commit('UPDATE CONFIG SET config_value = "{0}" WHERE config_field = "{1}"'.format(config_value, config_field))
+        except Exception as e:
+            Logger.log('ERROR', e)
+
+    def get_user_state(self, module, user_id):
+        try:
+            res = self.execute('SELECT state FROM ModuleState WHERE module = {0} and user_id = {1}'.format(module, user_id))
+            return res.fetchall()
+        except Exception as e:
+            Logger.log('ERROR', e)
+
+    def set_user_state(self, module, user_id, state):
+        try:
+            self.execute_and_commit('INSERT OR REPLACE INTO ModuleStates (module, user_id, state) VALUES ("{0}", "{1}", "{2}")'.format(module, user_id, state))
         except Exception as e:
             Logger.log('ERROR', e)
